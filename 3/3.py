1033d5b91dca439a3ed92cfc97a70fecccaff49d
hwkim301 <hwhw31@naver.com>
2025-06-06 17:18:57 +0900

    formatted with black

--- 3/3.py
+++ 3/3.py
@@ -4,6 +4,6 @@ import re
 res = requests.get("http://www.pythonchallenge.com/pc/def/equality.html")
 boundary = r"<!--(.*?)-->"
 
-mess = ''.join(re.findall(boundary, res.text,re.DOTALL))
-answer=''.join(re.findall(r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]',mess))
-print(answer) # linkedlist
+mess = "".join(re.findall(boundary, res.text, re.DOTALL))
+answer = "".join(re.findall(r"[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]", mess))
+print(answer)  # linkedlist
