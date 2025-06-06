23048d100acc106c7b43a567cc77f182e5ab6041
hwkim301 <hwhw31@naver.com>
2025-06-06 12:33:28 +0900

    added comments

--- 1/1.py
+++ 1/1.py
@@ -15,3 +15,5 @@ def dec(cipher):
 
 ciphertext = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
 print(dec(ciphertext))
+
+# print(dec("map")) 
