9969f94af7f041931d62c1a4a474188a6244f54b
hwkim301 <hwhw31@naver.com>
2025-06-06 12:23:09 +0900

    changed image folder location

---
+++ 1/1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj." | tr a-z c-za-x
