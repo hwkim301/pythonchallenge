d435ad980aa6de8a81ea4884f21da4ffe3f88bbd
hwkim301 <hwhw31@naver.com>
2025-06-06 13:29:41 +0900

    commented out string concatenation and incorporated the str.join method

--- 1/1.py
+++ 1/1.py
@@ -3,14 +3,24 @@ import string
 letters = string.ascii_lowercase
 
 
+# def dec(cipher):
+#     decrypt = ""
+#     for c in cipher:
+#         if c in letters:
+#             decrypt += chr((ord(c) - ord("a") + 2) % 26 + 97)
+#         else:
+#             decrypt += c
+#     return decrypt
+
+
 def dec(cipher):
-    decrypt = ""
+    decrypt = []
     for c in cipher:
         if c in letters:
-            decrypt += chr((ord(c) - ord("a") + 2) % 26 + 97)
+            decrypt.append(chr((ord(c) - ord("a") + 2) % 26 + 97))
         else:
-            decrypt += c
-    return decrypt
+            decrypt.append(c)
+    return "".join(decrypt)
 
 
 ciphertext = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
