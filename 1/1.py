efdd50b61bec06aa3465e544bddb6111e1b949d0
hwkim301 <hwhw31@naver.com>
2025-06-06 12:24:49 +0900

    python code formatting using black

--- 1/1.py
+++ 1/1.py
@@ -1,14 +1,17 @@
-import string 
+import string
 
-letters=string.ascii_lowercase
+letters = string.ascii_lowercase
+
 
 def dec(cipher):
-    decrypt=''
+    decrypt = ""
     for c in cipher:
         if c in letters:
-            decrypt+=chr((ord(c)-ord('a')+2)%26+97)
-        else: decrypt+=c 
+            decrypt += chr((ord(c) - ord("a") + 2) % 26 + 97)
+        else:
+            decrypt += c
     return decrypt
 
-ciphertext="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
+
+ciphertext = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
 print(dec(ciphertext))
