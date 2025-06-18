7c508c4bb29c3fa5d98f6927e752282673e24190
hwkim301 <hwhw31@naver.com>
2025-06-18 02:28:42 +0000

    Got rid of black formatting because I didn't like it

--- 20/20.py
+++ 20/20.py
@@ -2,55 +2,31 @@ import requests
 import zipfile 
 
 nextval = 30203
-res = requests.get(
-    "http://www.pythonchallenge.com/pc/hex/unreal.jpg",
-    auth=("butter", "fly"),
-    headers={"Range": "bytes=30203-2123456789"},
-)
+res = requests.get("http://www.pythonchallenge.com/pc/hex/unreal.jpg",auth=("butter", "fly"),headers={"Range": "bytes=30203-2123456789"})
 print(res.text, len(res.text))
 
 nextval = nextval + len(res.text)
-res = requests.get(
-    "http://www.pythonchallenge.com/pc/hex/unreal.jpg",
-    auth=("butter", "fly"),
-    headers={"Range": f"bytes={nextval}-2123456789"},
-)
+res = requests.get("http://www.pythonchallenge.com/pc/hex/unreal.jpg",auth=("butter", "fly"),headers={"Range": f"bytes={nextval}-2123456789"})
 print(res.text, len(res.text))
 
 
 while True:
     nextval = nextval + len(res.text)
-    res = requests.get(
-        "http://www.pythonchallenge.com/pc/hex/unreal.jpg",
-        auth=("butter", "fly"),
-        headers={"Range": f"bytes={nextval}-2123456789"},
-    )
+    res = requests.get("http://www.pythonchallenge.com/pc/hex/unreal.jpg",auth=("butter", "fly"),headers={"Range": f"bytes={nextval}-2123456789"})
     print(res.text)
     if len(res.text) == 0:
         break
 
-res = requests.get(
-    "http://www.pythonchallenge.com/pc/hex/unreal.jpg",
-    auth=("butter", "fly"),
-    headers={"Range": "bytes=2123456744-"},
-)
+res = requests.get("http://www.pythonchallenge.com/pc/hex/unreal.jpg",auth=("butter", "fly"),headers={"Range": "bytes=2123456744-"})
 print(res.text, len(res.text))
 print(res.headers)
 
 
-res = requests.get(
-    "http://www.pythonchallenge.com/pc/hex/unreal.jpg",
-    auth=("butter", "fly"),
-    headers={"Range": "bytes=2123456743-"},
-)
+res = requests.get("http://www.pythonchallenge.com/pc/hex/unreal.jpg", auth=("butter", "fly"),headers={"Range": "bytes=2123456743-"})
 print(res.text, len(res.text))
 print(res.headers)
 
-res = requests.get(
-    "http://www.pythonchallenge.com/pc/hex/unreal.jpg",
-    auth=("butter", "fly"),
-    headers={"Range": "bytes=1152983631-"},
-)
+res = requests.get("http://www.pythonchallenge.com/pc/hex/unreal.jpg",auth=("butter", "fly"),headers={"Range": "bytes=1152983631-"})
 print(res.text, len(res.text))
 print(res.headers)
 
