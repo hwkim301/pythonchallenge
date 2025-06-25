51fd7298fe35c0c653cb4dddf88af32a0b288fd3
hwkim301 <hwhw31@naver.com>
2025-06-25 10:10:55 +0900

    level28 solution

--- 28/28.py
+++ 28/28.py
@@ -4,11 +4,11 @@ with Image.open("bell.png") as img:
     r, g, b = img.split()
     greens = list(g.getdata())
 
-pairs=list(zip(greens[0::2],greens[1::2]))
+pairs = list(zip(greens[0::2], greens[1::2]))
 
 
-irregular=[abs(a-b) for a,b, in pairs if abs(a-b) !=42]
+irregular = [abs(a - b) for a, b, in pairs if abs(a - b) != 42]
 print(irregular)
 
-print(bytes(irregular)) # b'whodunnit().split()[0] ?'
-print("Guido Van Rossum".split()[0]) # Guido
+print(bytes(irregular))  # b'whodunnit().split()[0] ?'
+print("Guido Van Rossum".split()[0])  # Guido
