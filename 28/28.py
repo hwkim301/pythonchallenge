b6b1451f3dd6193cf2dd6ea184cfa82424fa39d9
hwkim301 <hwhw31@naver.com>
2025-06-25 10:10:23 +0900

    added some more lines

--- 28/28.py
+++ 28/28.py
@@ -9,3 +9,6 @@ pairs=list(zip(greens[0::2],greens[1::2]))
 
 irregular=[abs(a-b) for a,b, in pairs if abs(a-b) !=42]
 print(irregular)
+
+print(bytes(irregular)) # b'whodunnit().split()[0] ?'
+print("Guido Van Rossum".split()[0]) # Guido
